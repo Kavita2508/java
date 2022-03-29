e14ef8186e172e1095553edf76a11762fcb32b39
Kavita2508 <101554350+Kavita2508@users.noreply.github.com>
2022-03-29 17:19:49 +0530

    Add files via upload

---
+++ MInheritance.java
@@ -0,0 +1,28 @@
+
+public class MInheritance implements Printable,Showable{
+	public void print() {
+	
+	{
+		System.out.println("Hello");
+	}
+
+	}
+	public void show() {
+		{
+			System.out.println("Welcome");
+		}
+	}
+
+
+	public static void main(String[] args) {
+		MInheritance obj= new MInheritance();
+		obj.print();
+		obj.show();
+
+	}
+
+
+}
+
+	
+	
---
+++ methodoverriding.java
@@ -0,0 +1,23 @@
+
+public class methodoverriding {
+
+	public void chequebook () 
+	{
+		System.out.println("2 chequebooks");
+		
+	}
+public void phBanking()
+{
+	System.out.println("Yes");
+
+}
+
+public void atmcard()
+{
+	System.out.println("Yes");
+
+}
+
+	
+
+}
